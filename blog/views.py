f90c44295b6dfdcf1dc126c64956045f8cc8ed54
Abitesh <abiteshpabbulate@gmail.com>
2026-06-23 21:40:24 +0530

    Completed - Part-11 pagination

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,8 @@ from django.views.generic import (
     DeleteView
 )
 from django.urls import reverse_lazy
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 
 def home(request):
     context = {
@@ -26,6 +28,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
 
 class PostDetailView(DetailView):
     model = Post
@@ -60,3 +63,13 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         if self.request.user == post.author:
             return True
         return False
+    
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
