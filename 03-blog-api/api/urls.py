3c09fb56ffe1e09108e9eef6e25546855ebaa39f
Abitesh <abiteshpabbulate@gmail.com>
2026-06-27 12:56:22 +0530

    completed class based views - part 3 of DRF

--- 03-blog-api/api/urls.py
+++ 03-blog-api/api/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('posts/', views.post_list, name='post-list'),
-    path('posts/<int:pk>/', views.post_detail, name='post-detail'),
+    path('posts/', views.PostList.as_view(), name='post-list'),
+    path('posts/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
