ae1699edda742d13094664f9597adb758b8de635
Abitesh <abiteshpabbulate@gmail.com>
2026-06-27 12:20:10 +0530

    Part-2 of DRF request vs responses completed

--- 03-blog-api/api/urls.py
+++ 03-blog-api/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('posts/', views.post_list),
-    path('posts/<int:pk>/', views.post_detail),
+    path('posts/', views.post_list, name='post-list'),
+    path('posts/<int:pk>/', views.post_detail, name='post-detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- 03-blog-api/api/views.py
+++ 03-blog-api/api/views.py
@@ -1,50 +1,48 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .models import Post
 from .serializers import PostSerializer
 
-@csrf_exempt
-def post_list(request):
-
-    #List all posts, or create a new post.
-
-    if request.method == "GET":
+@api_view(['GET', 'POST'])
+def post_list(request, format=None):
+    """
+    List all posts, or create a new post.
+    """
+    if request.method == 'GET':
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
-    elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = PostSerializer(data=data)
+    elif request.method == 'POST':
+        serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-@csrf_exempt
-def post_detail(request, pk):
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def post_detail(request, pk, format=None):
     """
-    Retrieve, update or delete a post.
+    Retrieve, update or delete a post instance.
     """
     try:
         post = Post.objects.get(pk=pk)
     except Post.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == "GET":
+    if request.method == 'GET':
         serializer = PostSerializer(post)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
-    elif request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = PostSerializer(post, data=data)
+    elif request.method == 'PUT':
+        serializer = PostSerializer(post, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == "DELETE":
+    elif request.method == 'DELETE':
         post.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
